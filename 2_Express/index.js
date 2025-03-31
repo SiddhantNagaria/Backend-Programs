7d262a77af373d2bcea173fc9676a64c6e7b8c08
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-03-31 12:41:28 +0530

    handling request and sending repsonse

--- 2_Express/index.js
+++ 2_Express/index.js
@@ -1,9 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+// app.get('/', (req, res) => {
+//     res.send('Hello World!');
+// });
+
+//handling requests
+
+app.use((req, res) => {
+  //sending object
+
+  // res.send({
+  //     message: 'Hello World!',
+  //     status: 200
+  // });
+  let list = "<h1>List of something</h1><br/><li>Hi something</li>";
+  res.send(list);
 });
 
 app.listen(port, () => {
