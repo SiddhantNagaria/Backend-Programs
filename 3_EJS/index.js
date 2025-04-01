c5882454b3dd288615c596d3b4979a8f75b12a8d
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-04-01 22:34:39 +0530

    serving static files

--- 3_EJS/index.js
+++ 3_EJS/index.js
@@ -3,6 +3,10 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
+//serve static files
+app.use(express.static(path.join(__dirname, "/public/css")));
+app.use(express.static(path.join(__dirname, "/public/js")));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "./views"));
 
