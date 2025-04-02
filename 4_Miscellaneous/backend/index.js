2fe72bd0bd523d8910e11c953f460d6ade80b9c6
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-04-02 11:31:28 +0530

    handle post request

--- 4_Miscellaneous/backend/index.js
+++ 4_Miscellaneous/backend/index.js
@@ -3,14 +3,21 @@ const app = express();
 const port = 3000;
 
 
+// Middleware to parse JSON body
+app.use(express.json());
+
+// Middleware to parse URL-encoded body
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/register', (req, res) => {
     let {user,password} = req.query;
     res.send(`Standard GET Response ! Welcome ${user}`);
 }); 
 
 app.post('/register', (req, res) => {
+    let {user,password} = req.body;
     console.log(req.body);
-    res.send('Standard POST Response !');
+    res.send(`Standard POST Response !  Welcome ${user}`);
 }); 
 
 app.listen(port, () => {
