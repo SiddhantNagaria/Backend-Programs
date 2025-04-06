bdbcdfdfc326102284bfca3beed2adbd330792ac
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-04-06 22:21:54 +0530

    uuid

--- 5_REST/index.js
+++ 5_REST/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -11,31 +12,36 @@ app.use(express.static(path.join(__dirname, "./public")));
 
 let posts = [
     {
-        id: "1",
+        id: uuidv4(),
         username: "siddhant",
         content: "I do coding",
     },
     {
-        id: "2",
+        id: uuidv4(),
         username: "rahul",
         content: "I do video editing",
     },
 ];
 
+
+//index route
 app.get("/posts", (req, res) => {
     res.render("index.ejs", { posts });
 });
 
+//create route
 app.get("/posts/new", (req,res)=>{
     res.render("new.ejs");
 })
 
 app.post("/posts", (req, res) => {
     let { username, content } = req.body;
-    posts.push({ username, content }); //push as object
+    let id = uuidv4();
+    posts.push({ id, username, content }); //push as object
     res.redirect("/posts");
 })
 
+//show route
 app.get("/posts/:id", (req, res) => {
     let {id} = req.params;
     let post = posts.find((p)=> id === p.id);
