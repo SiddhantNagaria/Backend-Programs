9f2b5ff24610a671f4c2a4b9d88068b83ae4e3f2
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-04-07 16:50:20 +0530

    destroy route

--- 5_REST/index.js
+++ 5_REST/index.js
@@ -67,6 +67,15 @@ app.get("/posts/:id/edit", (req, res) => {
     res.render("edit.ejs", { post });
 });
 
+
+//delete route
+app.delete("/posts/:id",(req,res)=>{
+    let {id} = req.params;
+    posts = posts.filter((p) => id !== p.id);
+    res.redirect("/posts");
+
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
