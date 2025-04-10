bca5db1dc4daa12e0f014c39801b5b07b9c070ac
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-04-10 22:59:37 +0530

    mysql2 connection

--- 6_Backend_with_SQL/index.js
+++ 6_Backend_with_SQL/index.js
@@ -1,4 +1,24 @@
 const { faker } = require("@faker-js/faker");
+const mysql = require("mysql2"); //get client
+
+// Create the connection to database
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    database: "backend_sql",
+    password: "sknagaria",
+});
+
+try {
+    connection.query("SHOW TABLES", (err, res) => {
+        if (err) throw err;
+        console.log(res);
+    });
+} catch (err) {
+    console.log(err);
+};
+
+connection.end();
 
 let getRandomUser = () => {
     return {
@@ -9,5 +29,4 @@ let getRandomUser = () => {
     };
 };
 
-
-console.log(getRandomUser());
+// console.log(getRandomUser());
