bc7ea023ab941ab3425b7944f772eeefedc6978b
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-04-11 17:46:56 +0530

    home route

--- 6_Backend_with_SQL/index.js
+++ 6_Backend_with_SQL/index.js
@@ -1,12 +1,20 @@
 const { faker } = require("@faker-js/faker");
 const mysql = require("mysql2"); //get client
+const express = require("express");
+const app = express();
+const uuid = require("uuid");
+const port = 3000;
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "./views"));
 
 // Create the connection to database
 const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     database: "backend_sql",
-    password: "sknagaria",
+    password: "siddhant",
 });
 
 let getRandomUser = () => {
@@ -16,7 +24,8 @@ let getRandomUser = () => {
     //     email: faker.internet.email(),
     //     password: faker.internet.password(),
     // };
-    return [ // return array
+    return [
+        // return array
         faker.string.uuid(),
         faker.internet.username(),
         faker.internet.email(),
@@ -26,12 +35,11 @@ let getRandomUser = () => {
 
 let data = [];
 
-for (let i = 0; i < 100; i++) {
-    data.push(getRandomUser());
-}
+// for (let i = 0; i < 100; i++) {
+//     data.push(getRandomUser());
+// }
 
 // let q1 = "show tables";
-
 
 //inserting new data manually
 
@@ -43,20 +51,36 @@ let q3 = "insert into users (id, username, email, password) values ?";
 //     ["789", "sachin", "sachin@abc.com", "sachin"],
 // ];
 
+// try {
+//     connection.query(q3, [data], (err, res) => {
+//         if (err) throw err;
+//         console.log(res);
+//         // console.log(res.length);
+//         // console.log(res[0]);
+//         // console.log(res[1]);
+//     });
+// } catch (err) {
+//     console.log(err);
+// }
 
-
-try {
-    connection.query(q3, [data], (err, res) => {
-        if (err) throw err;
-        console.log(res);
-        // console.log(res.length);
-        // console.log(res[0]);
-        // console.log(res[1]);
-    });
-} catch (err) {
-    console.log(err);
-}
-
-connection.end();
+// connection.end();
 
 // console.log(getRandomUser());
+
+app.get("/", (req, res) => {
+    let q = `select count(*) from users`;
+    try {
+        connection.query(q, (err, result) => {
+            if (err) throw err;
+            let count = result[0]["count(*)"];
+            res.render("home.ejs", { count });
+        });
+    } catch (err) {
+        res.send("some error occured");
+        console.log(err);
+    }
+});
+
+app.listen(port, (req, res) => {
+    console.log(`server is listening to port ${port}`);
+});
