61dd15aa11ffd0e0285f1ff46377812428a222f0
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-05-16 11:35:30 +0530

    mongo setup with ejs

---
+++ 8_Mongo3/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const path = require('path');
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+main().then(()=>{
+    console.log('connected to MongoDB');
+}).catch(err=>{
+    console.log('Error connecting to MongoDB', err);
+});
+
+async function main(){
+    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+}
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(8080, (req,res)=>{
+    console.log("Server is running on port 8080");
+})
