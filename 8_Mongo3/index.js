ce8d86faebe17b7d1063200b978c690b9ae6831a
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-05-21 00:49:37 +0530

    async error

--- 8_Mongo3/index.js
+++ 8_Mongo3/index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const path = require('path');
 const Chat = require('./models/Chat.js');
 const methodOverride = require('method-override');
+const ExpressError = require("./ExpressError.js");
 
 
 app.set("views", path.join(__dirname, "views"));
@@ -32,6 +33,7 @@ app.get('/chats', async (req, res) => {
 
 //new route
 app.get('/chats/new', (req, res) => {
+    // throw new ExpressError(404, "Page not found");
     res.render('new.ejs');
 })
 
@@ -54,6 +56,16 @@ app.post('/chats', (req, res) => {
 });
 
 
+//show route
+app.get("/chats/:id", async (req, res, next) => {
+    let { id } = req.params;
+    let chat = await Chat.findById(id);
+    if (!chat) {
+        next(new ExpressError(404, "chat not found"));
+    }
+    res.render("edit.ejs", { chat })
+})
+
 //edit route
 app.get('/chats/:id/edit', async (req, res) => {
     let { id } = req.params;
@@ -65,7 +77,7 @@ app.get('/chats/:id/edit', async (req, res) => {
 //update route
 app.put('/chats/:id', async (req, res) => {
     let { id } = req.params;
-    let { msg : newMsg } = req.body;
+    let { msg: newMsg } = req.body;
     let updatedChat = await Chat.findByIdAndUpdate(id, { msg: newMsg },
         { runValidators: true, new: true });
     console.log(updatedChat);
@@ -80,12 +92,20 @@ app.delete('/chats/:id', async (req, res) => {
     console.log(deletedChat);
     console.log('Chat deleted successfully');
     res.redirect('/chats');
-    
+
 })
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
+
+
+//Error Handler Middleware
+app.use((err, req, res, next) => {
+    let { status = 500, message = "some error occured" } = err;
+    res.status(status).send(message);
+
+})
 
 app.listen(8080, (req, res) => {
     console.log("Server is running on port 8080");
