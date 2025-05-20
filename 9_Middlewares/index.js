5f5054f2dd5d8f4efecd9f80d54d5a926a8c77f2
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-05-20 21:39:10 +0530

    utility middleware

--- 9_Middlewares/index.js
+++ 9_Middlewares/index.js
@@ -13,6 +13,14 @@ app.use((req, res, next) => {
     next();
 })
 
+
+//utility middleware
+app.use((req,res,next)=>{
+    req.time = new Date(Date.now()).toString();
+    console.log(req.method, req.hostname, req.path, req.time);
+    next();
+})
+
 app.get("/", (req, res) => {
     res.send("hi i am root");
 })
