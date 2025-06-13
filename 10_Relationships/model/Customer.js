a3ec50f74c1e071a9a46797b7422536d6d346f83
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-06-13 22:33:41 +0530

    handling deletion

--- 10_Relationships/model/Customer.js
+++ 10_Relationships/model/Customer.js
@@ -32,25 +32,52 @@ const customerSchema = new Schema({
 const Order = mongoose.model('Order', orderSchema);
 const Customer = mongoose.model('Customer', customerSchema);
 
-const addCustomer = async () => {
-    let cust1 = new Customer({
-        name: "Siddhant Nagaria",
-    });
-    let order1 = await Order.findOne({ item: "chips" });
-    let order2 = await Order.findOne({ item: "chocolate" });
-    cust1.orders.push(order1);
-    cust1.orders.push(order2);
+// const addCustomer = async () => {
+//     let cust1 = new Customer({
+//         name: "Siddhant Nagaria",
+//     });
+//     let order1 = await Order.findOne({ item: "chips" });
+//     let order2 = await Order.findOne({ item: "chocolate" });
+//     cust1.orders.push(order1);
+//     cust1.orders.push(order2);
 
-    let res = await cust1.save();
-    console.log(res);
-}
+//     let res = await cust1.save();
+//     console.log(res);
+// }
 
-addCustomer();
+// addCustomer();
 
 const find = async()=>{
     let res = await Customer.findOne({name:"Siddhant Nagaria"}).populate("orders");
     console.log(res);
 } 
+
+
+const addCust = async()=>{
+    let newCust = new Customer({
+        name:"Siddhant",
+    });
+    let newOrder = new Order({
+        item:"doll",
+        price:6000
+    });
+    newCust.orders.push(newOrder);
+    await newOrder.save();
+    await newCust.save();
+
+    console.log("added new customer");
+
+};
+
+addCust();
+
+
+const delCust = async()=>{
+    let data = await Customer.findByIdAndDelete("683d6fa5791d09ffba1242b3");
+    console.log(data);
+}
+
+delCust();
 
 find();
 
